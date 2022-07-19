8667c48550f9c841d9a0cf8c3745374b5f543392
nilimabhoyar <109583593+nilimabhoyar@users.noreply.github.com>
2022-07-19 13:48:34 +0530

    Add files via upload

---
+++ javascriptrefresher.js
@@ -0,0 +1,71 @@
+// git clone https://github.com/abhigun1234/refersherjscssbootstarp.git
+//js 
+
+// variable declaration 
+
+
+//es5
+// 
+var a='1';
+console.log("a",a)
+
+//operators  arthemetic operator  + - / *  
+
+// mname=prompt('enter your first name')
+// console.log("mname",mname)
+
+// var a=Number(prompt('enter first no'))
+// var b=Number(prompt('enter second no'))
+// // add
+// var result=a+b
+// console.log("result",result)
+//div
+//mul
+//exp
+
+// comperision operator
+// == ===
+// a=23
+// b='23'
+// console.log("a==b",a==b)
+// console.log("a===b",a===b)
+
+// null undifined
+// console.log(typeof(null))
+
+// console.log(typeof(undefined))
+
+// var a
+// var a=null
+// if(a==null && a==undefined){
+
+// }
+
+//input three no and check which one is greater
+
+var a=Number(prompt('enter first no'))
+var b=Number(prompt('enter second no'))
+var c=Number(prompt('enter third no'))
+price=2500
+// logiic operator
+if(a>b ){
+
+console.log("a is greater")
+}
+else if(b>c){
+    console.log("b is greater")
+
+}
+else{
+    console.log("c is greater")
+
+}
+
+
+
+
+
+
+
+
+
